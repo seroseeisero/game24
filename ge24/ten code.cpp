7e88599453e2b8377af7448eed0e59846cb6f623
goddamnshit <pongthitheethawat@gmail.com>
2025-02-20 17:28:19 +0700

    Update ten code.cpp

--- ge24/ten code.cpp
+++ ge24/ten code.cpp
@@ -24,21 +24,45 @@ class Game {
 public:
     vector<Player> players;
 
-    Game() {
-        for (int i=0; i<4; ++i) {
+    Game(int numPlayers) {  
+        for (int i = 0; i < numPlayers; ++i) {
             string playerName;
-            cout <<"Enter name for Player " << (i + 1) << ": ";
+            cout << "Enter name for Player " << (i + 1) << ": ";
             cin >> playerName;
             players.push_back(Player(playerName));
         }
     }
+
     void startGame() {
         cout << "\nGame Started!" << endl;
     }
 };
+
 int main() {
-    Game game;
+    string really;
+    cout << " --  --*--ARE--*--  --" << endl;
+    cout << " --  --*--YOU--*--  --  " << endl;
+    cout << "--- --*--REALLY--*-- ---    " << endl;
+    cout << "REALLY??? :";
+    cin >> really;
+
+    int numPlayers;
+    cout << "Enter number of players (1-4): ";
+    cin >> numPlayers;
+
+    if (numPlayers < 1 || numPlayers > 4) {
+        cout << "Invalid number of players. Please enter a number between 1 and 4." << endl;
+        return 1;
+    }
+
+    Game game(numPlayers);  
     game.startGame();
+
+  
+    for (int i = 0; i < numPlayers; i++) {
+        game.players[i].displayInfo();
+    }
 
     return 0;
 }
+
